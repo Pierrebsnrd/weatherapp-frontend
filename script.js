3a624bddab972c9377418a706a5e76ba78426709
Pierre Boisnard <pierre.boisnard@live.fr>
2025-01-26 15:27:27 +0100

    update frontend

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ fetch('https://weatherapp-frontend-dun.vercel.app/')
 function updateDeleteCityEventListener() {
 	for (let i = 0; i < document.querySelectorAll('.deleteCity').length; i++) {
 		document.querySelectorAll('.deleteCity')[i].addEventListener('click', function () {
-			fetch(`https://weatherapp-frontend-dun.vercel.app/weather/${this.id}`, { method: 'DELETE' })
+			fetch(`https://weatherapp-frontend-dun.vercel.app/${this.id}`, { method: 'DELETE' })
 				.then(response => response.json())
 				.then(data => {
 					if (data.result) {
@@ -38,7 +38,7 @@ function updateDeleteCityEventListener() {
 document.querySelector('#addCity').addEventListener('click', function () {
 	const cityName = document.querySelector('#cityNameInput').value;
 
-	fetch('https//weatherapp-frontend-dun.vercel.app/weather/', {
+	fetch('https//weatherapp-frontend-dun.vercel.app/', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify({ cityName }),
