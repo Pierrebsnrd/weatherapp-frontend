327bc8e06978c676a7d488d29864ec5303d9a163
Pierre Boisnard <pierre.boisnard@live.fr>
2025-01-26 14:37:44 +0100

    update frontend

---
+++ login.js
@@ -0,0 +1,32 @@
+document.querySelector('#register').addEventListener('click', function() {
+    const email = document.querySelector('#registerEmail').value;
+    const name = document.querySelector('#registerName').value;
+    const password = document.querySelector('#registerPassword').value;
+    
+    fetch('http://localhost:3000/users/signup/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({email, name, password})
+    })
+    .then(response => response.json())
+    .then(data => {
+            console.log(data)
+            window.location.assign('index.html')
+    })
+})
+
+document.querySelector('#connection').addEventListener('click', function() {
+    const email = document.querySelector('#registerEmail').value;
+    const password = document.querySelector('#registerPassword').value;
+    
+    fetch('http://localhost:3000/users/signin/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({email, password})
+    })
+    .then(response => response.json())
+    .then(data => {
+            console.log(data)
+            window.location.assign('index.html')
+    })
+})
